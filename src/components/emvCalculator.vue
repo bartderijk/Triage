e68a9c6de9fedaf00e3ff29bd88b5bced340b8d2
bartderijk <derijkbart@gmail.com>
2019-09-30 17:36:37 +0200

    Added grid, removed default app styles

--- src/components/emvCalculator.vue
+++ src/components/emvCalculator.vue
@@ -1,34 +1,46 @@
 <template>
-<div>
-  <label for="eye">Eye Opening Response</label>
-  <select id="eye" v-model.number="eye">
-    <option value="4">Spontaneous–open with blinking at baseline (4)</option>
-    <option value="3">Opens to verbal command, speech, or shout (3)</option>
-    <option value="2">Opens to pain, not applied to face (2)</option>
-    <option value="1">None (1)</option>
-  </select><br>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1>EMV Score: {{ emvScore }}</h1>
+      </div>
+    </div>
+    <div class="row">
+      <div class="col-sm">
+        <label for="eye">Eye Opening Response</label><br>
+        <select id="eye" v-model.number="eye">
+          <option value="4">Spontaneous–open with blinking at baseline (4)</option>
+          <option value="3">Opens to verbal command, speech, or shout (3)</option>
+          <option value="2">Opens to pain, not applied to face (2)</option>
+          <option value="1">None (1)</option>
+        </select>
+      </div>
 
-  <label for="movement">Verbal Response</label>
-  <select id="movement" v-model.number="movement">
-    <option value="5">Oriented (5)</option>
-    <option value="4">Confused conversation, but able to answer questions (4)</option>
-    <option value="3">Inappropriate responses, words discernible (3)</option>
-    <option value="2">Incomprehensible speech (2)</option>
-    <option value="1">None (1)</option>
-  </select><br>
-
-  <label for="verbal">Motor Response</label>
-  <select id="verbal" v-model.number="verbal">
-    <option value="6">Obeys commands for movement (6)</option>
-    <option value="5">Purposeful movement to painful stimulus (5)</option>
-    <option value="4">Withdraws from pain (4)</option>
-    <option value="3">Abnormal (spastic) flexion, decorticate posture (3)</option>
-    <option value="2">Extensor (rigid) response, decerebrate posture (2)</option>
-    <option value="1">None (1)</option>
-  </select>
-
-  <h1>{{ emvScore }}</h1>
+      <div class="col-sm">
+        <label for="movement">Verbal Response</label><br>
+        <select id="movement" v-model.number="movement">
+          <option value="5">Oriented (5)</option>
+          <option value="4">Confused conversation, but able to answer questions (4)</option>
+          <option value="3">Inappropriate responses, words discernible (3)</option>
+          <option value="2">Incomprehensible speech (2)</option>
+          <option value="1">None (1)</option>
+        </select>
+      </div>
+        
+      <div class="col-sm">
+        <label for="verbal">Motor Response</label><br>
+        <select id="verbal" v-model.number="verbal">
+          <option value="6">Obeys commands for movement (6)</option>
+          <option value="5">Purposeful movement to painful stimulus (5)</option>
+          <option value="4">Withdraws from pain (4)</option>
+          <option value="3">Abnormal (spastic) flexion, decorticate posture (3)</option>
+          <option value="2">Extensor (rigid) response, decerebrate posture (2)</option>
+          <option value="1">None (1)</option>
+        </select>
+      </div>
+  </div>
 </div>
+
 </template>
 
 <script>
@@ -48,5 +60,7 @@ export default {
 </script>
 
 <style scoped>
-
+  select { 
+    max-width: 260px; 
+  }
 </style>
