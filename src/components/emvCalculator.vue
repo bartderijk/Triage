a832010de7c5bb94b6dd1baca7f507600889b5ef
bartderijk <derijkbart@gmail.com>
2019-10-01 21:53:51 +0200

    Added i18n, replaced copy to be a bit shorter.

--- src/components/emvCalculator.vue
+++ src/components/emvCalculator.vue
@@ -2,40 +2,40 @@
   <div class="container">
     <div class="row">
       <div class="col">
-        <h1>EMV Score: {{ emvScore }}</h1>
+        <h1>{{ $t("emvScore", { score: emvScore }) }}</h1>
       </div>
     </div>
     <div class="row">
       <div class="col-sm">
-        <label for="eye">Eye Opening Response</label><br>
+        <label for="eye">{{ $t("eye", { score: eye }) }}</label><br>
         <select id="eye" v-model.number="eye">
-          <option value="4">Spontaneous–open with blinking at baseline (4)</option>
-          <option value="3">Opens to verbal command, speech, or shout (3)</option>
-          <option value="2">Opens to pain, not applied to face (2)</option>
-          <option value="1">None (1)</option>
+          <option value="4">{{ $t("spontaneous") }}</option>
+          <option value="3">{{ $t("verbal") }}</option>
+          <option value="2">{{ $t("pain") }}</option>
+          <option value="1">{{ $t("noReaction") }}</option>
         </select>
       </div>
 
       <div class="col-sm">
-        <label for="movement">Verbal Response</label><br>
-        <select id="movement" v-model.number="movement">
-          <option value="5">Oriented (5)</option>
-          <option value="4">Confused conversation, but able to answer questions (4)</option>
-          <option value="3">Inappropriate responses, words discernible (3)</option>
-          <option value="2">Incomprehensible speech (2)</option>
-          <option value="1">None (1)</option>
+        <label for="motor">{{ $t("motor", { score: motor }) }}</label><br>
+        <select id="motor" v-model.number="motor">
+          <option value="6">{{ $t("obeysCommands") }}</option>
+          <option value="5">{{ $t("localisesPain") }}</option>
+          <option value="4">{{ $t("painWithdrawal") }}</option>
+          <option value="3">{{ $t("painFlexion") }}</option>
+          <option value="2">{{ $t("painExtension") }}</option>
+          <option value="1">{{ $t("noReaction") }}</option>
         </select>
       </div>
-        
+
       <div class="col-sm">
-        <label for="verbal">Motor Response</label><br>
+        <label for="verbal">{{ $t("verbal", { score: verbal }) }}</label><br>
         <select id="verbal" v-model.number="verbal">
-          <option value="6">Obeys commands for movement (6)</option>
-          <option value="5">Purposeful movement to painful stimulus (5)</option>
-          <option value="4">Withdraws from pain (4)</option>
-          <option value="3">Abnormal (spastic) flexion, decorticate posture (3)</option>
-          <option value="2">Extensor (rigid) response, decerebrate posture (2)</option>
-          <option value="1">None (1)</option>
+          <option value="5">{{ $t("oriented") }}</option>
+          <option value="4">{{ $t("confused") }}</option>
+          <option value="3">{{ $t("inappropriateResponses") }}</option>
+          <option value="2">{{ $t("incomprehensibleSpeech") }}</option>
+          <option value="1">{{ $t("noReaction") }}</option>
         </select>
       </div>
   </div>
@@ -48,12 +48,12 @@ export default {
   name: 'emvCalculator',
   data: () => ({
     eye: 4,
-    movement: 5,
-    verbal: 6
+    motor: 6,
+    verbal: 5
   }),
   computed: {
     emvScore() {
-      return this.eye + this.movement + this.verbal
+      return this.eye + this.motor + this.verbal
     }
   }
 }
