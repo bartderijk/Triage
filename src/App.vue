78fde9faaf232c7fb07ca43e77abd976f0d7bb52
bartderijk <derijkbart@gmail.com>
2019-10-23 14:58:37 +0200

    Added RTS component setup, paving the way for it's functionality

--- src/App.vue
+++ src/App.vue
@@ -1,16 +1,20 @@
 <template>
   <div id="app">
     <emvCalculator/>
+    <hr>
+    <rtsCalculator/>
   </div>
 </template>
 
 <script>
 import emvCalculator from './components/emvCalculator.vue'
+import rtsCalculator from './components/rtsCalculator.vue'
 
 export default {
   name: 'app',
   components: {
-    emvCalculator
+    emvCalculator,
+    rtsCalculator
   }
 }
 </script>
---
+++ src/components/rtsCalculator.vue
@@ -0,0 +1,21 @@
+<template>
+  <div class="container">
+    <div class="row">
+      <div class="col">
+        <h1 class="app-title">{{ $t("rtsScore", { score: 12 }) }}</h1>
+      </div>
+    </div>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'rtsCalculator',
+}
+</script>
+
+<style scoped>
+  .app-title {
+    margin: 16px 0;
+  }
+</style>
