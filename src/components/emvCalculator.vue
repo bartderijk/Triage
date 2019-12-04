6201b949f0ddfbd99e906cdb64fe4dc778cb46ad
bartderijk <derijkbart@gmail.com>
2019-12-04 12:11:41 +0100

    More Bulma templating

--- src/components/emvCalculator.vue
+++ src/components/emvCalculator.vue
@@ -1,40 +1,53 @@
 <template>
   <section class="section">
-    <div class="container">
-        <h1 class="title">{{ $t("emvScore", { score: emvScore }) }}</h1>
-    </div>
-    <div class="container">
-        <label for="eye">{{ $t("labels.eye", { score: eye }) }}</label><br>
-        <select id="eye" v-model.number="eye">
-          <option value="4">{{ $t("spontaneous") }}</option>
-          <option value="3">{{ $t("verbal") }}</option>
-          <option value="2">{{ $t("pain") }}</option>
-          <option value="1">{{ $t("noReaction") }}</option>
-        </select>
-    </div>
-    <div class="container">
-        <label for="motor">{{ $t("labels.motor", { score: motor }) }}</label><br>
-        <select id="motor" v-model.number="motor">
-          <option value="6">{{ $t("obeysCommands") }}</option>
-          <option value="5">{{ $t("localisesPain") }}</option>
-          <option value="4">{{ $t("painWithdrawal") }}</option>
-          <option value="3">{{ $t("painFlexion") }}</option>
-          <option value="2">{{ $t("painExtension") }}</option>
-          <option value="1">{{ $t("noReaction") }}</option>
-        </select>
-    </div>
-    <div class="container">
-        <label for="verbal">{{ $t("labels.verbal", { score: verbal }) }}</label><br>
-        <select id="verbal" v-model.number="verbal">
-          <option value="5">{{ $t("oriented") }}</option>
-          <option value="4">{{ $t("confused") }}</option>
-          <option value="3">{{ $t("inappropriateResponses") }}</option>
-          <option value="2">{{ $t("incomprehensibleSpeech") }}</option>
-          <option value="1">{{ $t("noReaction") }}</option>
-        </select>
-    </div>
-</section>
-
+      <div class="container">
+        <h1 class="title">
+          {{ $t("emvScore", { score: emvScore }) }}
+        </h1>
+      </div>
+      <div class="field">
+          <label class="label" for="eye">{{ $t("labels.eye", { score: eye }) }}</label>
+          <div class="control">
+            <div class="select is-primary">
+              <select id="eye" v-model.number="eye">
+                <option value="4">{{ $t("spontaneous") }}</option>
+                <option value="3">{{ $t("verbal") }}</option>
+                <option value="2">{{ $t("pain") }}</option>
+                <option value="1">{{ $t("noReaction") }}</option>
+              </select>
+            </div>
+          </div>
+      </div>
+      <div class="field">
+          <label class="label" for="motor">{{ $t("labels.motor", { score: motor }) }}</label>
+          <div class="control">
+            <div class="select is-primary">
+              <select id="motor" v-model.number="motor">
+                <option value="6">{{ $t("obeysCommands") }}</option>
+                <option value="5">{{ $t("localisesPain") }}</option>
+                <option value="4">{{ $t("painWithdrawal") }}</option>
+                <option value="3">{{ $t("painFlexion") }}</option>
+                <option value="2">{{ $t("painExtension") }}</option>
+                <option value="1">{{ $t("noReaction") }}</option>
+              </select>
+            </div>
+          </div>
+      </div>
+      <div class="field">
+        <label class="label" for="verbal">{{ $t("labels.verbal", { score: verbal }) }}</label>
+        <div class="control">
+          <div class="select is-primary">
+            <select id="verbal" v-model.number="verbal">
+              <option value="5">{{ $t("oriented") }}</option>
+              <option value="4">{{ $t("confused") }}</option>
+              <option value="3">{{ $t("inappropriateResponses") }}</option>
+              <option value="2">{{ $t("incomprehensibleSpeech") }}</option>
+              <option value="1">{{ $t("noReaction") }}</option>
+            </select>
+          </div>
+        </div>
+      </div>
+  </section>
 </template>
 
 <script>
@@ -54,12 +67,7 @@ export default {
 </script>
 
 <style scoped>
-  /* .app-title {
-    margin: 16px 0;
+  .title {
+    margin-bottom: 16px; 
   }
-
-  select { 
-    min-width: 260px;
-    height: 32px;
-  } */
 </style>
