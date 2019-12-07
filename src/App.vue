71740b3cc0a2f7a20b6611b0ddd859b1be7dc3dc
bartderijk <derijkbart@gmail.com>
2019-12-07 19:55:48 +0100

    Added footer with licence info

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,5 @@
 <template>
+<div>
   <div class="columns">
     <div class="column">
       <emvCalculator/>
@@ -7,6 +8,16 @@
       <rtsCalculator/>
     </div>
   </div>
+  <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Triage</strong> by <a href="https://bartderijk.com">Bart de Rijk</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The application content
+        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      </p>
+    </div>
+</footer>
+</div>
 </template>
 
 <script>
