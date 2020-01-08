6878b13557112adc519741ba9b738cf7a4d014b5
bartderijk <derijkbart@gmail.com>
2020-01-08 13:52:01 +0100

    Moved footer to its own component

--- src/App.vue
+++ src/App.vue
@@ -20,37 +20,33 @@
       </div>
     </div>
   </nav>
+  
   <div class="columns calculator">
     <div class="column">
-      <emvCalculator/>
+      <EmvCalculator/>
     </div>
     <hr>
     <div class="column">
-      <rtsCalculator/>
+      <RtsCalculator/>
     </div>
   </div>
-  <footer class="footer">
-    <div class="content has-text-centered">
-      <p>
-        <strong>Triage</strong> by <a href="https://bartderijk.com">Bart de Rijk</a>. The source code is licensed
-        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The application content
-        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
-      </p>
-    </div>
-  </footer>
+  
+  <Footer/>
 </div>
 </template>
 
 <script>
-import emvCalculator from './components/emvCalculator.vue'
-import rtsCalculator from './components/rtsCalculator.vue'
+import EmvCalculator from './components/emvCalculator.vue'
+import RtsCalculator from './components/rtsCalculator.vue'
+import Footer from './components/footer.vue'
 import { mapGetters } from 'vuex';
 
 export default {
   name: 'app',
   components: {
-    emvCalculator,
-    rtsCalculator
+    EmvCalculator,
+    RtsCalculator,
+    Footer
   },
   computed: {
     ...mapGetters(['gcs', 'rts']),
---
+++ src/components/footer.vue
@@ -0,0 +1,17 @@
+<template>
+    <footer class="footer">
+    <div class="content has-text-centered">
+      <p>
+        <strong>Triage</strong> by <a href="https://bartderijk.com">Bart de Rijk</a>. The source code is licensed
+        <a href="http://opensource.org/licenses/mit-license.php">MIT</a>. The application content
+        is licensed <a href="http://creativecommons.org/licenses/by-nc-sa/4.0/">CC BY NC SA 4.0</a>.
+      </p>
+    </div>
+  </footer>
+</template>
+
+<script>
+export default {
+  name: 'Footer',
+}
+</script>
