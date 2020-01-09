caee5712ed4ebbd6c53251311399efb15fab7b0d
bartderijk <derijkbart@gmail.com>
2020-01-09 13:50:11 +0100

    Split up the store so its externally available for testing

--- src/store/index.js
+++ src/store/index.js
@@ -3,37 +3,43 @@ import Vuex from 'vuex'
 
 Vue.use(Vuex)
 
-export default new Vuex.Store({
-  state: {
-    eye: 4,
-    motor: 6,
-    verbal: 5,
-    sbpRtsScore: 4,
-    rrRtsScore: 4,
-    gcsRtsScore: 4
+export const state = {
+  eye: 4,
+  motor: 6,
+  verbal: 5,
+  sbpRtsScore: 4,
+  rrRtsScore: 4,
+  gcsRtsScore: 4
+};
+
+export const getters = {
+  gcs: state => state.eye + state.motor + state.verbal,
+  rts: state => state.sbpRtsScore + state.rrRtsScore + state.gcsRtsScore
+};
+
+export const mutations = {
+  setEye(state, score) {
+    state.eye = score;
   },
-  getters: {
-    gcs: state => state.eye + state.motor + state.verbal,
-    rts: state => state.sbpRtsScore + state.rrRtsScore + state.gcsRtsScore
+  setMotor(state, score) {
+    state.motor = score;
   },
-  mutations: {
-    setEye(state, score) {
-      state.eye = score;
-    },
-    setMotor(state, score) {
-      state.motor = score;
-    },
-    setVerbal(state, score) {
-      state.verbal = score;
-    },
-    setSbpRtsScore(state, score) {
-      state.sbpRtsScore = score;
-    },
-    setRrRtsScore(state, score) {
-      state.rrRtsScore = score;
-    },
-    setGcsRtsScore(state, score) {
-      state.gcsRtsScore = score;
-    }
+  setVerbal(state, score) {
+    state.verbal = score;
+  },
+  setSbpRtsScore(state, score) {
+    state.sbpRtsScore = score;
+  },
+  setRrRtsScore(state, score) {
+    state.rrRtsScore = score;
+  },
+  setGcsRtsScore(state, score) {
+    state.gcsRtsScore = score;
   }
+};
+
+export default new Vuex.Store({
+  state,
+  getters,
+  mutations
 })
