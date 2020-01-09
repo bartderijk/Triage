f28ac346eed24e626b43b0b62feb05ec0435e0b6
bartderijk <derijkbart@gmail.com>
2020-01-09 17:34:47 +0100

    Added unit test for rts

--- tests/unit/store.spec.js
+++ tests/unit/store.spec.js
@@ -1,14 +1,20 @@
 import { getters } from '@/store';
 
-describe('getters', () => {
-  it('GCS is a the total sum of eye, motor and verbal', () => {
-    const state = {
-      eye: 4,
-      motor: 6,
-      verbal: 5,
-    };
+const state = {
+  eye: 4,
+  motor: 6,
+  verbal: 5,
+  sbpRtsScore: 4,
+  rrRtsScore: 4,
+  gcsRtsScore: 4
+};
 
-    expect(getters.gcs(state))
-    .toEqual(state.eye + state.motor + state.verbal);
+describe('getters', () => {
+  it('Returns the glascow coma scale (gcs) based on the total of eye, motor and verbal', () => {
+    expect(getters.gcs(state)).toEqual(state.eye + state.motor + state.verbal);
+  }),
+
+  it('Returns the revised trauma score (rts) based on the total of gcs, respiratory rate and blood pressure', () => {
+    expect(getters.rts(state)).toEqual(state.gcsRtsScore + state.rrRtsScore + state.sbpRtsScore);
   })
 })
